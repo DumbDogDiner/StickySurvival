56c4f2524575dd7e2669d960395661fa9788bb8b
Matt <25674682+spazzylemons@users.noreply.github.com>
2021-02-18 21:29:52 -0500

    Improve /sgsetup border, fix perms etc for setup, add helpful errors

--- src/main/kotlin/com/dumbdogdiner/stickysurvival/command/SGSetupCommand.kt
+++ src/main/kotlin/com/dumbdogdiner/stickysurvival/command/SGSetupCommand.kt
@@ -26,6 +26,8 @@ import com.dumbdogdiner.stickysurvival.util.schedule
 import com.sk89q.worldedit.WorldEdit
 import com.sk89q.worldedit.bukkit.BukkitPlayer
 import com.sk89q.worldedit.bukkit.BukkitWorld
+import com.sk89q.worldedit.extension.platform.permission.ActorSelectorLimits
+import com.sk89q.worldedit.math.BlockVector3
 import org.bukkit.configuration.file.YamlConfiguration
 import org.bukkit.entity.Player
 
@@ -42,7 +44,7 @@ private val spawnAdd = cmd("add") { sender, _, _ ->
                 "pitch" to sender.location.pitch,
             )
             it.set("spawn points", spawnPointsYaml)
-        }
+        } ?: return@cmd ExitCode.EXIT_ERROR_SILENT
         sender.sendMessage("Spawn point added successfully.")
         ExitCode.EXIT_SUCCESS
     } catch (e: Exception) {
@@ -50,7 +52,7 @@ private val spawnAdd = cmd("add") { sender, _, _ ->
         sender.sendMessage(e.message ?: return@cmd ExitCode.EXIT_ERROR)
         ExitCode.EXIT_ERROR_SILENT
     }
-}
+}.requiresPlayer().permission("stickysurvival.setup")
 
 private val spawnList = cmd("list") { sender, _, _ ->
     sender as Player
@@ -60,14 +62,14 @@ private val spawnList = cmd("list") { sender, _, _ ->
             it.getList("spawn points")?.withIndex()?.forEach { (i, spawnPoint) ->
                 sender.sendMessage("${i + 1}: $spawnPoint")
             }
-        }
+        } ?: return@cmd ExitCode.EXIT_ERROR_SILENT
         ExitCode.EXIT_SUCCESS
     } catch (e: Exception) {
         e.printStackTrace()
         sender.sendMessage(e.message ?: return@cmd ExitCode.EXIT_ERROR)
         ExitCode.EXIT_ERROR_SILENT
     }
-}
+}.requiresPlayer().permission("stickysurvival.setup")
 
 private val spawnRemove = cmd("remove", int("index")) { sender, args, _ ->
     sender as Player
@@ -82,7 +84,7 @@ private val spawnRemove = cmd("remove", int("index")) { sender, args, _ ->
                 it.set("spawn points", list)
                 success = true
             }
-        }
+        } ?: return@cmd ExitCode.EXIT_ERROR_SILENT
         if (success) {
             sender.sendMessage("Spawn point removed successfully.")
             ExitCode.EXIT_SUCCESS
@@ -95,20 +97,38 @@ private val spawnRemove = cmd("remove", int("index")) { sender, args, _ ->
         sender.sendMessage(e.message ?: return@cmd ExitCode.EXIT_ERROR)
         ExitCode.EXIT_ERROR_SILENT
     }
-}
+}.requiresPlayer().permission("stickysurvival.setup")
 
 private val spawn = cmdStub("spawn")
     .subCommand(spawnAdd)
     .subCommand(spawnList)
     .subCommand(spawnRemove)
+    .requiresPlayer()
+    .permission("stickysurvival.setup")
 
-private val border = cmd("border") { sender, _, _ ->
+private val borderGet = cmd("get") { sender, _, _ ->
+    sender as Player
+    val worldEdit = WorldEdit.getInstance()
+    val session = worldEdit.sessionManager[BukkitPlayer(sender)]
+    val selector = session.getRegionSelector(BukkitWorld(sender.world))
+    withConfig(sender, mutates = false) {
+        val limits = ActorSelectorLimits.forActor(BukkitPlayer(sender))
+        val point1 = BlockVector3.at(it.getInt("bounds.min.x"), it.getInt("bounds.min.y"), it.getInt("bounds.min.z"))
+        val point2 = BlockVector3.at(it.getInt("bounds.max.x"), it.getInt("bounds.max.y"), it.getInt("bounds.max.z"))
+        selector.selectPrimary(point1, limits)
+        selector.selectSecondary(point2, limits)
+    } ?: return@cmd ExitCode.EXIT_ERROR_SILENT
+    sender.sendMessage("The border has been loaded into the region.")
+    ExitCode.EXIT_SUCCESS
+}.requiresPlayer().permission("stickysurvival.setup")
+
+private val borderSet = cmd("set") { sender, _, _ ->
     sender as Player
     val worldEdit = WorldEdit.getInstance()
     val world = BukkitWorld(sender.world)
     val session = worldEdit.sessionManager[BukkitPlayer(sender)]
     if (!session.isSelectionDefined(world)) {
-        sender.sendMessage("You don't have a region selected! (Use WorldEdit)")
+        sender.sendMessage("You don't have a region selected!")
         ExitCode.EXIT_ERROR_SILENT
     } else {
         val selection = session.getSelection(world)
@@ -121,11 +141,21 @@ private val border = cmd("border") { sender, _, _ ->
             it.set("bounds.max.x", max.x)
             it.set("bounds.max.y", max.y)
             it.set("bounds.max.z", max.z)
-        }
-        sender.sendMessage("The bounds have been updated.")
+        } ?: return@cmd ExitCode.EXIT_ERROR_SILENT
+        sender.sendMessage("The border has been updated.")
         ExitCode.EXIT_SUCCESS
     }
-}
+}.requiresPlayer().permission("stickysurvival.setup")
+
+private val border = cmdStub("border")
+    .subCommand(borderGet)
+    .subCommand(borderSet)
+    .onExecute { sender, _, _ ->
+        sender.sendMessage("This command is now /sgsetup border set")
+        ExitCode.EXIT_ERROR_SILENT
+    }
+    .requiresPlayer()
+    .permission("stickysurvival.setup")
 
 val sgSetupCommandBuilder: BukkitCommandBuilder = cmdStub("sgsetup")
     .requiresPlayer()
@@ -133,6 +163,10 @@ val sgSetupCommandBuilder: BukkitCommandBuilder = cmdStub("sgsetup")
     .onTabComplete { _, _, args ->
         val argArray = args.rawArgs.toTypedArray()
         when {
+            argArray.matches("border", ANY) -> {
+                setOf("get", "set").filter { it.startsWith(argArray[1]) }.toMutableList()
+            }
+
             argArray.matches("spawn", ANY) -> {
                 setOf("add", "list", "remove").filter { it.startsWith(argArray[1]) }.toMutableList()
             }
@@ -147,15 +181,18 @@ val sgSetupCommandBuilder: BukkitCommandBuilder = cmdStub("sgsetup")
     .subCommand(border)
     .subCommand(spawn)
 
-private fun withConfig(player: Player, mutates: Boolean = true, block: (YamlConfiguration) -> Unit) {
+private fun withConfig(player: Player, mutates: Boolean = true, block: (YamlConfiguration) -> Unit): Unit? {
     val name = player.world.name
-    // this is good enough
-    // if (name !in worlds) {
-    //     throw IllegalArgumentException("No such world $name - make a config file for this world first and reload!")
-    // }
     val file = StickySurvival.instance.dataFolder.resolve("worlds").resolve("$name.yml")
-    if (!file.exists()) throw IllegalArgumentException("config file not exist")
+    if (!file.exists()) {
+        player.sendMessage("The config file doesn't exist! You'll have to create a stub yourself. (TODO add a config wizard)")
+        return null
+    }
     val config = YamlConfiguration.loadConfiguration(file)
+    if (config.getKeys(true).isEmpty()) {
+        player.sendMessage("Config file was empty when reading. Check the console! If the file couldn't be opened, try restarting the server.")
+        return null
+    }
 
     block(config)
 
@@ -167,4 +204,6 @@ private fun withConfig(player: Player, mutates: Boolean = true, block: (YamlConf
             }
         }
     }
+
+    return Unit // is this smart? is this dumb? whatever this is, this is what happens when i code at 9 pm
 }
