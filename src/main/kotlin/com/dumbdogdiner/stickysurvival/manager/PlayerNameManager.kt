51cbad259a91388ec4fac9cf2ebea70fe7da09f9
Matt <25674682+spazzylemons@users.noreply.github.com>
2021-03-28 16:06:36 -0400

    Use Ashcon for player name cache + comments galore to said cache

--- src/main/kotlin/com/dumbdogdiner/stickysurvival/manager/PlayerNameManager.kt
+++ src/main/kotlin/com/dumbdogdiner/stickysurvival/manager/PlayerNameManager.kt
@@ -22,38 +22,65 @@ import com.dumbdogdiner.stickyapi.common.cache.Cache
 import com.dumbdogdiner.stickyapi.common.cache.Cacheable
 import com.google.gson.Gson
 import org.bukkit.Bukkit
-import java.io.IOException
 import java.net.URL
 import java.util.UUID
 
+/**
+ * Caches player names for use in systems that require the names of potentially offline players, such as leaderboards.
+ */
 object PlayerNameManager {
-    class CachedPlayerName(val uuid: UUID, val name: String?) : Cacheable {
+    /** Maps a UUID to a username. */
+    data class UUIDToUsername(val uuid: UUID, val username: String) : Cacheable {
         override fun getKey() = uuid.toString()
     }
 
-    data class NameHistory(val name: String, val changedToAt: Long?)
+    /** Cache of entries */
+    private val cache = Cache(UUIDToUsername::class.java)
 
-    private val cache = Cache(CachedPlayerName::class.java)
-
+    /**
+     * Get the username for a player by their UUID, or null if it cannot be obtained.
+     */
     operator fun get(uuid: UUID): String? {
-        val cached = cache[uuid.toString()]
-        return if (cached == null) {
-            Bukkit.getPlayer(uuid)?.name ?: run {
-                val name = try {
-                    URL("https://api.mojang.com/user/profiles/${uuid.toString().replace("-", "")}/names").openStream()
-                } catch (e: IOException) {
-                    e.printStackTrace()
-                    return null
-                }.use {
-                    Gson().fromJson(it.reader(), Array<NameHistory>::class.java)
-                }.apply {
-                    sortByDescending { it.changedToAt ?: 0 }
-                }.firstOrNull()?.name
-                cache.put(CachedPlayerName(uuid, name))
-                name
+        // Check the cache first
+        return cache[uuid.toString()]?.username
+            // Was not in the cache, try Bukkit next
+            ?: checkBukkit(uuid)?.username
+            // Was not in Bukkit's offline player storage, ask Ashcon
+            ?: try {
+                // Attempt to query Ashcon for the information
+                queryAshcon(uuid).username
+            } catch (e: Exception) {
+                // If all else fails, return null
+                e.printStackTrace()
+                null
             }
-        } else {
-            cached.name
+    }
+
+    /**
+     * Attempt to get the username from Bukkit's OfflinePlayer system.
+     */
+    private fun checkBukkit(uuid: UUID): UUIDToUsername? {
+        Bukkit.getOfflinePlayer(uuid).name?.let {
+            // Bukkit knows this player's name, cache the entry and return it
+            val entry = UUIDToUsername(uuid, it)
+            cache.put(entry)
+            return@checkBukkit entry
         }
+        // Not found, return null
+        return null
+    }
+
+    /**
+     * Make a query to Ashcon for a given UUID.
+     */
+    private fun queryAshcon(uuid: UUID): UUIDToUsername {
+        // Make query
+        val response = URL("https://api.ashcon.app/mojang/v2/user/$uuid").openStream().use {
+            // Use Gson to serialize what we need
+            Gson().fromJson(it.reader(), UUIDToUsername::class.java)
+        }
+        // Cache the response and return it
+        cache.put(response)
+        return response
     }
 }
