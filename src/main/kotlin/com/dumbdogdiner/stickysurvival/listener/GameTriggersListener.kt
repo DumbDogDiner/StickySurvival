e34e61f7bf1c55e0a95fbd07bc4cc280486b9d9c
James <jcahill773@gmail.com>
2021-04-19 19:40:44 +0100

    Initial PoC for unbinding the worldedit wand when in-game

--- src/main/kotlin/com/dumbdogdiner/stickysurvival/listener/GameTriggersListener.kt
+++ src/main/kotlin/com/dumbdogdiner/stickysurvival/listener/GameTriggersListener.kt
@@ -3,10 +3,17 @@ package com.dumbdogdiner.stickysurvival.listener
 import com.dumbdogdiner.stickysurvival.event.TributeAddEvent
 import com.dumbdogdiner.stickysurvival.event.TributeRemoveEvent
 import com.dumbdogdiner.stickysurvival.util.messages
+import com.sk89q.worldedit.LocalSession
+import com.sk89q.worldedit.WorldEdit
+import com.sk89q.worldedit.bukkit.BukkitAdapter
+import com.sk89q.worldedit.command.tool.SelectionWand
+import com.sk89q.worldedit.world.item.ItemType
 import com.thevoxelbox.voxelsniper.VoxelProfileManager
 import org.bukkit.Bukkit
+import org.bukkit.Material
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.inventory.ItemStack
 
 object GameTriggersListener : Listener {
 
@@ -37,6 +44,25 @@ object GameTriggersListener : Listener {
         }
     }
 
+    // private val worldEditWandStorage = ConcurrentHashMap<UUID, Tool>()
+
+    private fun getWorldEditLocalSession(player: org.bukkit.entity.Player): LocalSession? {
+        // Make sure WorldEdit is actually enabled
+        if (Bukkit.getServer().pluginManager.isPluginEnabled("WorldEdit")) {
+            val actor = BukkitAdapter.adapt(player)
+            val sessionManager = WorldEdit.getInstance().sessionManager
+            return sessionManager.get(actor)
+        }
+        return null
+    }
+
+    private fun getWorldEditWoodenAxeItemType(): ItemType {
+        // Get the ItemType for a wooden axe (default wg wand)
+        val item = ItemStack(Material.WOODEN_AXE)
+        val baseItemStack = BukkitAdapter.adapt(item)
+        return baseItemStack.type
+    }
+
     @EventHandler
     fun onTributeAdd(event: TributeAddEvent) {
         (getVoxelProfileManager())?.let { voxelProfileManager ->
@@ -49,6 +75,25 @@ object GameTriggersListener : Listener {
             // Inform the player that their sniper has been disabled
             event.player.sendMessage(messages.voxelsniper.disabled)
         }
+
+        // WorldEdit
+        (
+            getWorldEditLocalSession(event.player)?.let { localSession ->
+                val type = getWorldEditWoodenAxeItemType()
+
+                // If gettool returns a value (ie. one is set) add it to our map for safekeeping
+                // localSession.getTool(type)?.let { worldEditWandStorage.put(event.player.uniqueId, it) }
+
+                event.player.sendMessage("Found tool of type: " + type.richName)
+                event.player.sendMessage("Found getTool of: " + localSession.getTool(type))
+
+                event.player.sendMessage("Matches wand item?" + type.id.equals(localSession.wandItem))
+
+                event.player.sendMessage("Unbinding tool...")
+                localSession.setTool(type, null)
+                event.player.sendMessage("tool unbinded")
+            }
+            )
     }
 
     @EventHandler
@@ -62,6 +107,25 @@ object GameTriggersListener : Listener {
 
             // Inform the player that their sniper has been disabled
             event.player.sendMessage(messages.voxelsniper.enabled)
+        }
+
+        (getWorldEditLocalSession(event.player))?.let { localSession ->
+            // Re-bind to wooden axe
+            val type = getWorldEditWoodenAxeItemType()
+
+            event.player.sendMessage("Found tool of type: " + type.richName)
+            event.player.sendMessage("Found getTool of: " + localSession.getTool(type))
+
+            event.player.sendMessage("Matches wand item?" + type.id.equals(localSession.wandItem))
+
+            localSession.setTool(type, SelectionWand())
+
+            event.player.sendMessage("Re-bound selectionWand to wooden pick! Re-checking...")
+
+            event.player.sendMessage("Found tool of type: " + type.richName)
+            event.player.sendMessage("Found getTool of: " + localSession.getTool(type))
+
+            event.player.sendMessage("Matches wand item?" + type.id.equals(localSession.wandItem))
         }
     }
 }
